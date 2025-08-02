685de8b7779be1b65b853b5968e9b0794b502ae3
javadzamanifard <javadzamanii.1374@gmail.com>
2025-08-02 05:29:37 -0700

    Implement password change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import homeview, CustomSignUpView
+from .views import homeview, CustomSignUpView, CustomPasswordChangeView
 
 urlpatterns = [
     path('home/', homeview, name='home'),
     path('signup/', CustomSignUpView.as_view(), name='signup'),
+    path('accounts/password/change/', CustomPasswordChangeView.as_view(), name='account_change_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from allauth.account.views import PasswordChangeView
+from django.contrib import messages
 
 from .forms import CustomSignUpForm
 
@@ -14,4 +16,12 @@ class CustomSignUpView(generic.CreateView):
     template_name = 'account/signup.html'
     context_object_name = 'form'
     success_url = reverse_lazy('home')
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('home')
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'رمز عبور با موفیت تغییر یافت')
+        return super().form_valid(form)
 
