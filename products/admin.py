62c807b077d3af28e5174513f101d0e13dc4a95a
javadzamanifard <javadzamanii.1374@gmail.com>
2025-08-04 05:57:07 -0700

    Implement products admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)} # اسلاگ به صورت خودکار از روی نام پر می‌شود
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'created_at']
+    list_filter = ['created_at', 'category']
+    list_editable = ['price', 'stock',] # امکان ویرایش مستقیم از لیست
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name', 'description']
+
+
